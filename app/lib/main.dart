5ac92bcf02c39f7319aebc15f4cb8b137d144e57
tidalcurrent <sidneysteward@gmail.com>
2024-10-03 21:13:44 -0400

    theme change

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(CalculatorApp());
@@ -7,8 +8,43 @@ void main() {
 class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const appName = 'Calculator';
+
     return MaterialApp(
-      home: CalculatorScreen(),
+      title: appName,
+      theme: ThemeData(
+        useMaterial3: true,
+
+        // Define the default brightness and colors.
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          // TRY THIS: Change to "Brightness.light"
+          //           and see that all colors change
+          //           to better contrast a light background.
+          brightness: Brightness.dark,
+        ),
+
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          ),
+          // TRY THIS: Change one of the GoogleFonts
+          //           to "lato", "poppins", or "lora".
+          //           The title uses "titleLarge"
+          //           and the middle text uses "bodyMedium".
+          titleLarge: GoogleFonts.oswald(
+            fontSize: 30,
+            fontStyle: FontStyle.italic,
+          ),
+          bodyMedium: GoogleFonts.merriweather(),
+          displaySmall: GoogleFonts.pacifico(),
+        ),
+      ),
+      home: CalculatorScreen(
+      ),
     );
   }
 }
